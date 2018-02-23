80d98fc9aa9fcab5f9b0595f53f7788601aa594e
Dima Spitsyn <sqvoch@gmail.com>
2018-02-23 19:15:25 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,30 +1,46 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.array = [];
   }
 
   add(element) {
     // your implementation
+    this.array.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.array[index];
   }
 
   get length() {
     // your implementation
+    return this.array.length;
   }
 
   toArray() {
     // your implementation
+    return this.array;
   }
 
   sort(indices) {
     // your implementation
+    let arr = [];
+    indices.sort();
+    for (let i = 0; i < indices.length; i++) {
+      arr.push(this.array[indices[i]]);
+    }
+    arr.sort(this.compareFunction || ((a, b) => a - b));
+
+    for (let i = 0; i < indices.length; i++) {
+      this.array[indices[i]] = arr[i]
+    }
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
